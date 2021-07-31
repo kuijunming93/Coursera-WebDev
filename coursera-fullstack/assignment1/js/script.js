07303f88c88e1cfffcc7cedafaaeb6e31764309d
kuijunming93 <kuijunming93@gmail.com>
2021-07-31 23:10:46 +0800

    Added project assignment for fullstack coursera course

---
+++ coursera-fullstack/assignment1/js/script.js
@@ -0,0 +1,9 @@
+$(function () { //download jQuery snippet for vscode  + this is nothing of document.addEventListener("DOMContentLoaded")
+
+    $("#navbarToggle").blur(function(event){ //same as document.querySelector("navbarToggle").addEventListener("blur")
+        var screenWidth =  window.innerWidth;
+        if (screenWidth<768){
+            $("#myNavbar").collapse('hide'); //part of JQuery func
+        }
+    }); 
+});
