efdf66ba2b36d8843df05841c250eb72454d1d08
kuijunming93 <kuijunming93@gmail.com>
2021-08-02 22:23:03 +0800

    Major update in BS5 webpage

--- site-playBS5/js/script.js
+++ site-playBS5/js/script.js
@@ -1,9 +1,9 @@
 $(function () { //download jQuery snippet for vscode  + this is nothing of document.addEventListener("DOMContentLoaded")
 
-    $("#navbarToggle").blur(function(event){ //same as document.querySelector("navbarToggle").addEventListener("blur")
+    $("#myNavTogglebtn").blur(function(event){ //same as document.querySelector("navbarToggle").addEventListener("blur")
         var screenWidth =  window.innerWidth;
         if (screenWidth<768){
-            $("#collapsable-nav").collapse('hide'); //part of JQuery func
+            $("#myNavbar").collapse('hide'); //part of JQuery func
         }
-    }); 
+    });
 });
