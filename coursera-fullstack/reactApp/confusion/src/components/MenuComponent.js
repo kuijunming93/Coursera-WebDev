9b4a275c76e5f2d3d2e700db7a664b16aa39575f
kuijunming93 <kuijunming93@gmail.com>
2021-11-28 18:03:33 +0800

    Revert "Added solution for assignment week 1"
    This reverts commit ab4c1c74031102c1df40a7dc92b0cef5b3b9f924.

--- coursera-fullstack/reactApp/confusion/src/components/MenuComponent.js
+++ coursera-fullstack/reactApp/confusion/src/components/MenuComponent.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishdetailComponent from "./DishdetailComponent";
 
 class Menu extends Component{
     constructor(props){
@@ -12,6 +11,23 @@ class Menu extends Component{
 
     onDishSelect(dish) {
         this.setState({selectedDish : dish});
+    }
+
+    renderDish(dish){
+        if (dish!=null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return (
+                <div></div>
+            );
     }
 
     render(){
@@ -35,7 +51,9 @@ class Menu extends Component{
                     {menu}
                 </div>
                 <div className="row">
-                    <DishdetailComponent selectedDish = {this.state.selectedDish}/>
+                  <div  className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.state.selectedDish)}
+                  </div>
                 </div>
             </div>
         );
