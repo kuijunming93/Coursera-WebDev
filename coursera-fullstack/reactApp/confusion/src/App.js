cee76e0ec39d272de9a512a6fbd8ac65de18c4ad
kuijunming93 <kuijunming93@gmail.com>
2021-11-29 21:59:53 +0800

    Added react-router into webApp

--- coursera-fullstack/reactApp/confusion/src/App.js
+++ coursera-fullstack/reactApp/confusion/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import {DISHES} from "./shared/dishes";
 import { Component } from 'react';
 import Main from "./components/MainComponent";
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component{
 
   render(){
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- coursera-fullstack/reactApp/confusion/src/components/MainComponent.js
+++ coursera-fullstack/reactApp/confusion/src/components/MainComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -16,16 +18,23 @@ class Main extends Component {
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
   render() {
+    const HomePage = () => {
+      return (
+        <Home />
+      );
+    }
+
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to="/home" />
+          </Switch>
+        {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail selectedDish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
         <Footer />
       </div>
     );
